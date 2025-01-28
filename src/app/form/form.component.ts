3f8e1094d4e242ae126b75a609df4d2a2cc786ab
Dave Henson <david.henson@bootsdigitalhealth.co.uk>
2025-01-28 09:26:51 +0000

    Add  form functionlaity

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+  styleUrl: './form.component.css',
 })
 export class FormComponent {
+  amount!: number;
+  term!: number;
+  rate!: number;
+  type!: string;
 
+  handleResultsFormSubmit() {
+    console.log(this.amount, this.term, this.rate, this.type);
+  }
 }
