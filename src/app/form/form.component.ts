431b8c03df731149d6535a40f2c91bb9bccfb991
Dave Henson <david.henson@bootsdigitalhealth.co.uk>
2025-03-04 10:29:40 +0000

    Add math functions

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -8,12 +8,23 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './form.component.css',
 })
 export class FormComponent {
-  amount!: number;
-  term!: number;
-  rate!: number;
-  type!: string;
+  amount: number = 0;
+  term: number = 0;
+  rate: number = 0;
+  type: string = 'repayment';
 
   handleResultsFormSubmit() {
     console.log(this.amount, this.term, this.rate, this.type);
+  }
+
+  calculateRepayment(): number {
+    return (
+      (this.amount * (this.rate / 12) * Math.pow(1 + this.rate / 12, this.term * 12)) /
+       Math.pow(1 + this.rate / 12, this.term * 12) - 1
+    );
+  }
+
+  calcualteInterest(): number {
+    return this.amount * (this.rate / 12);
   }
 }
